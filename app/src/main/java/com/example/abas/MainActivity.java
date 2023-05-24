f5459e550df045e50c7021b86b07c15b5020026a
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-24 01:23:47 -0300

    Criando projeto e add dependência SmartTabLayout

---
+++ app/src/main/java/com/example/abas/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.abas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*
+*  Vamos utilizar as bibliotecas do SmartTabLayout para criar as abas para o nosso projeto. No gitHub
+* existe uma pequena documentação exemplificando e explicando as possibilidades de layouts com
+* utilização de abas - link abaixo
+*
+* https://github.com/ogaclejapan/SmartTabLayout/tree/master/demo
+* */
