58a3f314100564dc49584eb5d803748c00f0561c
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-24 01:41:29 -0300

    Referenciando os componentes SmartTabLayout e PageView

--- app/src/main/java/com/example/abas/MainActivity.java
+++ app/src/main/java/com/example/abas/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.abas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
     }
 }
 
@@ -19,4 +28,6 @@ public class MainActivity extends AppCompatActivity {
 * utilização de abas - link abaixo
 *
 * https://github.com/ogaclejapan/SmartTabLayout/tree/master/demo
+*
+* Vamos criar referencias para os componentes SmartTabLayout e PageView no MainActivity.java
 * */
